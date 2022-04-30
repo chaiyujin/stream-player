2b0cd3e35ac2a50a574793b38c4b105a9d8c13ca
yuki-chai <chaiyujin@gmail.com>
2022-04-30 18:52:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,49 +15,46 @@ def test_imgui(window):
     imgui.style_colors_dark()
     imgui.impl_init(window)
 
-    io = imgui.get_io()
-    io.ini_filename = "imgui_new.ini"
-
-    is_open = np.asarray([True], dtype=np.uint8)
-    v_float = np.asarray([0.0], dtype=np.float32)
-    v_float2 = np.asarray([0.0, 1.0], dtype=np.float32)
-
     img_tex = ImageTexture()
     reader = VideoReader(os.path.expanduser("~/Videos/30fps.mp4"))
+    reader.seek_msec(0.0)
+    got, im = reader.read()
+    if got:
+        img_tex.update(im, is_bgr=True)
+    msec = np.asarray([0.0], dtype=np.float32)
 
-    count = 0
-    while not glfw.window_should_close(window):
+    def _frame_begin():
         glfw.poll_events()
-
-        got, im = reader.read()
-        if got:
-            img_tex.update(im, is_bgr=True)
-
         imgui.impl_new_frame()
         imgui.new_frame()
 
-        imgui.demo(is_open)
-        imgui.begin("haha", np.asarray([1], dtype=np.uint8))
-        imgui.text("Hello World!")
-        imgui.slider_float("Value", v_float, -10.0, 10.0)
-        imgui.slider_float2("Value2", v_float2, -10.0, 10.0)
-        imgui.checkbox("open", is_open)
-        imgui.image(img_tex.id, img_tex.extend)
-        imgui.end()
-
-        imgui.begin("test", is_open)
-        if imgui.button("Click {}".format(count)):
-            count += 1
-        imgui.end()
-
+    def _frame_end():
         imgui.render()
         w, h = glfw.get_framebuffer_size(window)
         gl.glViewport(0, 0, w, h)
         gl.glClearColor(0.0, 0.2, 0.2, 0.0)
         gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
         imgui.impl_render(imgui.get_draw_data())
-
         glfw.swap_buffers(window)
+
+    while not glfw.window_should_close(window):
+        _frame_begin()
+
+        # seek
+        reader.seek_msec(msec[0])
+        got, im = reader.read()
+        if got:
+            img_tex.update(im, is_bgr=True)
+
+        # draw
+        imgui.begin("Video", np.asarray([1], dtype=np.uint8))
+        imgui.text("Msec: {:.0f}ms, Frame: {:.0f}".format(msec[0], msec[0] * 30.0 / 1000.0))
+        if not img_tex.empty():
+            imgui.image(img_tex.id, img_tex.extend)
+        imgui.slider_float("Value", msec, 0.0, 10000.0, "%.0fms")
+        imgui.end()
+
+        _frame_end()
 
     imgui.impl_shutdown()
     imgui.destroy_context(imgui_ctx)
